3af5634d1f9b1044b362461805c227ee5c095711
gary.archer <gary.archer@WORKMAC.local>
2022-01-11 19:38:49 +0100

    WIP updates

---
+++ resources/nginx-plus/deploy_and_test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+##################################################################################
+# Automate the deployment and testing from here, using a test Nginx Plus account
+# https://docs.nginx.com/nginx/admin-guide/installing-nginx/installing-nginx-plus/
+##################################################################################
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+echo 'Not yet implemented ...'
